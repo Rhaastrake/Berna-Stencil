95de55dfc784728c60cda4e6f2929293c969dc99
Rhaastrake <garofalomichele2001@gmail.com>
2026-03-04 20:58:19 +0100

    Added base njk and custom css for every page

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,23 +1,23 @@
 module.exports = {
-  // site_name: "Site name",
-  // title: "Site title",
-  // description:
-  //   "Sie description",
-  // domain: "yoursite.com",
-  // url: "https://yoursite.com", // It WILL NOT end with a slash /
-  // lang: "en",
-  // author: "Name and surname",
-  // logo: "/assets/img/logo",
-  // image_width: [320, 480, 720, 1280, 1920, 2048, 2560, 3840, 4096, 7680],
-  // copyright: {
-  //   year: "0000",
-  //   text: "Copyright text",
-  //   privacy: "",//link to privacy policy
-  //   cookie: "",//link to cookies policy
-  //   cookieControls:
-  //     "",//link to cookies management
-  //   terms: "",//Link to terms and conditions
-  // },
+  site_name: "Site name",
+  title: "Site title",
+  description:
+    "Sie description",
+  domain: "yoursite.com",
+  url: "https://yoursite.com", // It WILL NOT end with a slash /
+  lang: "en",
+  author: "Name and surname",
+  logo: "/assets/img/logo",
+  image_width: [320, 480, 720, 1280, 1920, 2048, 2560, 3840, 4096, 7680],
+  copyright: {
+    year: "0000",
+    text: "Copyright text",
+    privacy: "",//link to privacy policy
+    cookie: "",//link to cookies policy
+    cookieControls:
+      "",//link to cookies management
+    terms: "",//Link to terms and conditions
+  },
   homepage: {
     seo: {
       title: "Homepage",
@@ -25,7 +25,7 @@ module.exports = {
       keywords: "First keyword, second keyword",
     },
   },
-  anotherPage: {
+  anotherpage: {
     seo: {
       title: "Another Page",
       description: "Description",
