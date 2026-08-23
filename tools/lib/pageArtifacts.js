59e1ee93482a46f8248dbe514942b32cda89272e
Rhaastrake <garofalomichele2001@gmail.com>
2026-08-23 20:44:21 +0200

    keep build output when removing a page

--- tools/lib/pageArtifacts.js
+++ tools/lib/pageArtifacts.js
@@ -3,18 +3,7 @@ const settings = require('../config/settings.json');
 const { PATHS } = require('./paths');
 const { exists } = require('./files');
 const { languageSettings, scriptPagesDirectory } = require('./project');
-const { getCurrentOutputPath } = require('./outputPath');
-const { toCamelCase, formatText } = require('./text');
-
-const PATH_SEPARATOR = '/';
-
-function outputRoot() {
-    return path.join(PATHS.root, getCurrentOutputPath() || settings.project.defaultOutputDirectory);
-}
-
-function resolveOutput(root, template, values) {
-    return path.join(root, ...formatText(template, values).split(PATH_SEPARATOR));
-}
+const { toCamelCase } = require('./text');
 
 function routeFile(pageName) {
     return path.join(PATHS.routes, `${pageName}.${settings.page.routeExtension}`);
@@ -26,8 +15,6 @@ function pageExists(pageName) {
 
 function getPageArtifacts(pageName) {
     const camelName = toCamelCase(pageName);
-    const values = { camelName, pageName };
-    const root = outputRoot();
 
     return {
         camelName,
@@ -35,10 +22,6 @@ function getPageArtifacts(pageName) {
             style:  path.join(PATHS.stylePages, `${camelName}.${settings.page.styleExtension}`),
             script: path.join(scriptPagesDirectory(), `${camelName}.${languageSettings().extension}`),
             route:  routeFile(pageName),
-        },
-        output: {
-            files:       settings.page.outputFiles.map((template) => resolveOutput(root, template, values)),
-            directories: settings.page.outputDirectories.map((template) => resolveOutput(root, template, values)),
         },
     };
 }
